079af3639067feb3b058e50e7d0ac78f0d8194a8
webllec <59725998+webllec@users.noreply.github.com>
2021-01-10 07:10:30 -0300

    Update Web Scraping Public API Receita Federal.py

--- Web Scraping Public API Receita Federal.py
+++ Web Scraping Public API Receita Federal.py
@@ -1,35 +1,34 @@
-# Auteur : Louis Leclerc
-# Creation date : 26/05/2020
-# Last update : 02/10/2020
-# Description :
-    # programme d'extraction des données d'entreprises brésiliennes (nom, activité, dirigeants, adresse...)
-    # à partir de leur numéro d'enregistrement (le CNPJ)
-    # et de l'API du registre fiscal fédéral (Receita Federal Web Services)
-    # plus d'info sur https://www.receitaws.com.br/api
+# Author: Louis Leclerc
+# Creation date: 26/05/2020
+# Last update: 02/10/2020
+# Description:
+    # scraper of Brazilian companies data (name, activity, directors, address...)
+    # input = tax registration number (CNPJ)
+    # from the Federal Tax Registry's API (Receita Federal Web Services)
+    # more info on https://www.receitaws.com.br/api
 
-# A faire:
-    # Retravailler la présentation des résultats dans la fonction finale
-    # Ou faire un CSV en sortie
-
-# Importation des modules requests, json, datetime et time, et localisation du fichier de CNPJ
 import requests
 import json
 import datetime
 import time
 import os
-os.chdir("/home/luchin/Documents/AmIntel/Communication/Blog/BDD Lava Jato")
 
-# définition des valeurs invariables
-my_file = "dados_amostra_CNPJ.csv"
+# defining file path
+# os.chdir("/FILE/PATH")
+
+# defining input file (has to be a one-column list of names in csv format)
+# my_file = "FILE_NAME.csv"
+
 url_receita = "https://www.receitaws.com.br/v1/cnpj/"
 error_message = "Invalid CNPJ"
-# token = {"Authorization":"token 607035d8e2dbd7e6b6b57234b3c905b44d6e73fd470e8d183993f0448c294dc2"}
-    # nécessaire uniquement pour la connexion payante à l'API
-# jour_j = datetime.date.today()-datetime.date(2020, 5, 26)
-    # nécessaire uniquement pour la connexion payante à l'API
-    # A retravailler pour avoir un nombre intégral de jours
 
-# fonction qui retourne une URL valide quelque soit la composition du CNPJ
+# token only necessary if using the paying services of the API (> 3 requests/s)
+# token = {"Authorization":"token 607035d8e2dbd7e6b6b57234b3c905b44d6e73fd470e8d183993f0448c294dc2"}
+
+# set d_day to today if using the paying API
+# d_day = datetime.date.today()-datetime.date(YEAR, MONTH, DAY)
+
+# function returning a valid API request URL from any CNPJ
 def correct_url(cnpj):
     if cnpj.isalnum()==False:
         new_cnpj= ""
@@ -39,20 +38,20 @@ def correct_url(cnpj):
             else:
                 pass
         if len(new_cnpj)==14:
-            return url_receita + new_cnpj # + "/days/" + jour_j pour l'API commerciale
+            return url_receita + new_cnpj # + "/days/" + d_day for the paying API
         else:
             return False
     elif cnpj.isalnum()==True and len(cnpj)==14:
-        return url_receita + cnpj # + "/days/" + jour_j pour l'API commerciale
+        return url_receita + cnpj # + "/days/" + + d_day for the paying API
     else:
         return False
 
-# fonction qui retourne une liste des données d'une entreprise à partir de son CNPJ
+# function returning a list of the company's data from its CNPJ
 def scraping_page(url):
     if url == False:
         return error_message
     else:
-        response = requests.get(url) #requests.get(url, headers=token) pour l'API commerciale
+        response = requests.get(url) #requests.get(url, headers=token) for the paying API
         page_content = response.text
         dct = json.loads(page_content)
         clefs= dct.keys()
@@ -61,19 +60,19 @@ def scraping_page(url):
         corpo_id = list(corpo_id)
         return corpo_id
 
-# fonction qui ouvre un CSV de CNPJ (sans autre info) et retourne une liste de ces CNPJ
+# function returning a CSV of CNPJs (without any other information) and returns a list of those CNPJs
 def file_cnpj(file):
     with open(file, encoding="utf-8") as f:
         res = f.read().splitlines()
     return res
 
-# fonction générale qui scrap les infos à partir d'une liste de CNPJ
-# en respectant la limite de 3 requêtes par minute de l'API publique
+# general function scrapping data from a list of CNPJs
+# respects the 3 request/sec limit of the free API
 def scrap_list(cnpj_list):
     request_nb = 0
     res=[]
     for i in cnpj_list:
-        if request_nb<3:
+        if request_nb<3: #change max number of requests if using the paying API
             res.append(scraping_page(correct_url(i)))
             request_nb+=1
         else:
